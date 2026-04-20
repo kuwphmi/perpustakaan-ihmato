9146ab336ba20cf67c35c912a561a867dbf93246
Reva <rizkyavrillita@gmail.com>
2026-04-20 12:47:32 +0700

    ...

--- frontend/src/pages/beranda.jsx
+++ frontend/src/pages/beranda.jsx
@@ -6,6 +6,7 @@ import logo from "../assets/logo.png"
 import { FiDollarSign, FiUser, FiBookOpen, FiShield } from "react-icons/fi"
 
 
+
 import banner1 from "../assets/banner1.png"
 import banner2 from "../assets/banner2.png"
 import banner3 from "../assets/banner3.png"
@@ -15,6 +16,7 @@ function beranda() {
   const [menuOpen, setMenuOpen] = useState(false)
   const [scrolled, setScrolled] = useState(false)
   const [currentSlide, setCurrentSlide] = useState(0)
+  const [activeMenu, setActiveMenu] = useState("beranda")
 
   useEffect(() => {
     AOS.init({
@@ -72,15 +74,41 @@ function beranda() {
 
         {/* DESKTOP */}
         <div className="hidden md:flex items-center gap-6 text-white">
-          <a href="#" className="text-blue-600 font-semibold">
-            Beranda
-          </a>
-          <a href="#" className="hover:text-blue-600 transition">
-            Keunggulan
-          </a>
-          <a href="#" className="hover:text-blue-600 transition">
-            Pustakawan
-          </a>
+          <a
+  href="#"
+  onClick={() => setActiveMenu("beranda")}
+  className={`transition ${
+    activeMenu === "beranda"
+      ? "text-blue-600 font-semibold"
+      : "text-white hover:text-blue-600"
+  }`}
+>
+  Beranda
+</a>
+
+<a
+  href="#keunggulan"
+  onClick={() => setActiveMenu("keunggulan")}
+  className={`transition ${
+    activeMenu === "keunggulan"
+      ? "text-blue-600 font-semibold"
+      : "text-white hover:text-blue-600"
+  }`}
+>
+  Keunggulan
+</a>
+
+<a
+  href="#"
+  onClick={() => setActiveMenu("pustakawan")}
+  className={`transition ${
+    activeMenu === "pustakawan"
+      ? "text-blue-600 font-semibold"
+      : "text-white hover:text-blue-600"
+  }`}
+>
+  Informasi
+</a>
 
           <button className="bg-blue-600 text-white px-4 py-2 rounded-2xl cursor-pointer transition-all duration-300 hover:bg-blue-700 hover:scale-105">
             Login
@@ -103,40 +131,77 @@ function beranda() {
 
       {/* MOBILE MENU */}
       <div
-        className={`fixed top-0 left-0 w-full h-full z-50 transform transition-transform duration-300 ${
-          menuOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-        style={{
-          backgroundColor: "#ffffff",
-          color: "#1f2937",
-        }}
-      >
-        <div className="flex items-center justify-between px-6 py-4 border-b">
-          <div className="flex items-center gap-2">
-            <img src={logo} className="w-8 h-8 object-contain" />
-            <h1 className="font-semibold text-blue-600">BukuIn</h1>
-          </div>
+  className={`fixed top-0 left-0 w-full h-full z-50 transform transition-transform duration-300 ${
+    menuOpen ? "translate-x-0" : "-translate-x-full"
+  }`}
+  style={{
+    backgroundColor: "#ffffff",
+    color: "#1f2937",
+  }}
+>
+  <div className="flex items-center justify-between px-6 py-4 border-b">
+    <div className="flex items-center gap-2">
+      <img src={logo} className="w-8 h-8 object-contain" />
+      <h1 className="font-semibold text-blue-600">BukuIn</h1>
+    </div>
 
-          <button
-            onClick={() => setMenuOpen(false)}
-            className="text-2xl cursor-pointer"
-          >
-            <FiX />
-          </button>
-        </div>
+    <button
+      onClick={() => setMenuOpen(false)}
+      className="text-2xl cursor-pointer"
+    >
+      <FiX />
+    </button>
+  </div>
 
-        <div className="flex flex-col items-start gap-6 px-6 py-10 text-lg">
-          <a href="#" className="text-blue-600 font-semibold cursor-pointer">
-            Beranda
-          </a>
-          <a href="#" className="hover:text-blue-600 transition cursor-pointer">
-            Buku user
-          </a>
-          <a href="#" className="hover:text-blue-600 transition cursor-pointer">
-            Pustakawan
-          </a>
-        </div>
-      </div>
+  <div className="flex flex-col items-start gap-6 px-6 py-10 text-lg">
+
+    <a
+      href="#"
+      onClick={() => {
+        setActiveMenu("beranda")
+        setMenuOpen(false)
+      }}
+      className={`transition ${
+        activeMenu === "beranda"
+          ? "text-blue-600 font-semibold"
+          : "hover:text-blue-600"
+      }`}
+    >
+      Beranda
+    </a>
+
+    <a
+      href="#keunggulan"
+      onClick={() => {
+        setActiveMenu("keunggulan")
+        setMenuOpen(false)
+      }}
+      className={`transition ${
+        activeMenu === "keunggulan"
+          ? "text-blue-600 font-semibold"
+          : "hover:text-blue-600"
+      }`}
+    >
+      Keunggulan
+    </a>
+
+    <a
+      href="#"
+      onClick={() => {
+        setActiveMenu("pustakawan")
+        setMenuOpen(false)
+      }}
+      className={`transition ${
+        activeMenu === "pustakawan"
+          ? "text-blue-600 font-semibold"
+          : "hover:text-blue-600"
+      }`}
+    >
+     Informasi
+    </a>
+
+  </div>
+</div>
 
       {/* BANNER SLIDER */}
       <div className="relative w-full h-[90vh] overflow-hidden mt-[-80px]">
@@ -251,26 +316,26 @@ function beranda() {
       </div>
 
       {/* 🔥 CONTENT SECTION */}
-<div className="bg-blue-50 py-20 px-6 md:px-16">
+<div id="keunggulan" className="bg-blue-50 py-20 px-6 md:px-16">
   
   <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center">
     
    {/* 🔥 KIRI (TEXT + IMAGE) */}
-<div className="ml-4 md:ml-12">
+<div className="md:ml-12 text-center md:text-left">
   
   <h2 className="text-3xl md:text-4xl font-bold text-gray-800 leading-snug">
     Digitalisasi Mudah untuk  <br /> Perpustakaanmu
   </h2>
 
-  {/* IMAGE */}
-  <div className="mt-6">
-    <img
-  src={foto}
-  alt="preview"
-  data-aos="fade-right"
-  className="custom-rotate-img w-[220px] md:w-[260px] h-[300px] md:h-[360px] object-cover rounded-2xl shadow-lg"
-/>
-  </div>
+{/* IMAGE */}
+<div className="mt-6 flex justify-center md:justify-start">
+  <img
+    src={foto}
+    alt="preview"
+    data-aos="fade-right"
+    className="custom-rotate-img w-[220px] md:w-[260px] h-[300px] md:h-[360px] object-cover rounded-2xl shadow-lg"
+  />
+</div>
 
 </div>
 
@@ -278,48 +343,64 @@ function beranda() {
    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:-ml-50 md:mt-25">
 
       {/* CARD 1 */}
-      <div className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition">
-        <FiDollarSign className="text-3xl text-blue-600 mb-3" />
-        <h3 className="font-semibold text-gray-800">
-          Hemat Biaya Operasional
-        </h3>
-        <p className="text-sm text-gray-500 mt-2">
-          Kelola perpustakaan tanpa perlu infrastruktur rumit seperti server atau ruang khusus.
-        </p>
-      </div>
+<div
+  data-aos="fade-up"
+  data-aos-delay="0"
+  className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition"
+>
+  <FiDollarSign className="text-3xl text-blue-600 mb-3" />
+  <h3 className="font-semibold text-gray-800">
+    Hemat Biaya Operasional
+  </h3>
+  <p className="text-sm text-gray-500 mt-2">
+    Kelola perpustakaan tanpa perlu infrastruktur rumit seperti server atau ruang khusus.
+  </p>
+</div>
 
-      {/* CARD 2 */}
-      <div className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition">
-        <FiUser className="text-3xl text-blue-600 mb-3" />
-        <h3 className="font-semibold text-gray-800">
-          Mudah Digunakan
-        </h3>
-        <p className="text-sm text-gray-500 mt-2">
-          Dirancang agar sederhana dan nyaman digunakan oleh siapa saja di berbagai perangkat.
-        </p>
-      </div>
+{/* CARD 2 */}
+<div
+  data-aos="fade-up"
+  data-aos-delay="100"
+  className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition"
+>
+  <FiUser className="text-3xl text-blue-600 mb-3" />
+  <h3 className="font-semibold text-gray-800">
+    Mudah Digunakan
+  </h3>
+  <p className="text-sm text-gray-500 mt-2">
+    Dirancang agar sederhana dan nyaman digunakan oleh siapa saja di berbagai perangkat.
+  </p>
+</div>
 
-      {/* CARD 3 */}
-      <div className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition">
-        <FiBookOpen className="text-3xl text-blue-600 mb-3" />
-        <h3 className="font-semibold text-gray-800">
-          Koleksi Lengkap & Variatif
-        </h3>
-        <p className="text-sm text-gray-500 mt-2">
-          Beragam pilihan konten tersedia untuk memenuhi kebutuhan bacaanmu.
-        </p>
-      </div>
+{/* CARD 3 */}
+<div
+  data-aos="fade-up"
+  data-aos-delay="200"
+  className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition"
+>
+  <FiBookOpen className="text-3xl text-blue-600 mb-3" />
+  <h3 className="font-semibold text-gray-800">
+    Koleksi Lengkap & Variatif
+  </h3>
+  <p className="text-sm text-gray-500 mt-2">
+    Beragam pilihan konten tersedia untuk memenuhi kebutuhan bacaanmu.
+  </p>
+</div>
 
-      {/* CARD 4 */}
-      <div className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition">
-        <FiShield className="text-3xl text-blue-600 mb-3" />
-        <h3 className="font-semibold text-gray-800">
-          Dukungan Penuh & Terpercaya
-        </h3>
-        <p className="text-sm text-gray-500 mt-2">
-          Didukung oleh tim profesional yang siap membantu kelancaran operasional.
-        </p>
-      </div>
+{/* CARD 4 */}
+<div
+  data-aos="fade-up"
+  data-aos-delay="300"
+  className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition"
+>
+  <FiShield className="text-3xl text-blue-600 mb-3" />
+  <h3 className="font-semibold text-gray-800">
+    Dukungan Penuh & Terpercaya
+  </h3>
+  <p className="text-sm text-gray-500 mt-2">
+    Didukung oleh tim profesional yang siap membantu kelancaran operasional.
+  </p>
+</div>
 
     </div>
   </div>
